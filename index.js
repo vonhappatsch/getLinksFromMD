02bb596fc06bd4d59bd745ff457d1c902927fa5e
Jéssica Happatsch <vonhappatsch@gmail.com>
2019-05-06 18:00:48 -0300

    início do projeto - função de extrair links

---
+++ index.js
@@ -0,0 +1,18 @@
+function getLinksFromMD(str) {
+  let matches = [];
+  const matcher = /\[([^\]]*)\]\(([^)]*)\)/g;
+  let result = '';
+
+  while(result = matcher.exec(str)) {
+    matches.push(result);
+  }
+  const finalMatches = matches.map(match => ({
+    href: match[2],
+    text: match[1]
+  }));
+  return finalMatches;
+}
+module.exports.getLinksFromMD = getLinksFromMD;
+
+
+
