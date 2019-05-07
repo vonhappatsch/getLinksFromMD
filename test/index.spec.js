85a277c3da463737e8564912cc42822d38ed8d07
Jéssica Happatsch <vonhappatsch@gmail.com>
2019-05-07 13:39:34 -0300

    nyc cobrindo todas as linhas

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,18 +4,28 @@ const chai = require('chai');
 const expect = chai.expect;
 describe('index test', () => {
   describe('valid link from string - only link', () => {
-    it('should return true', () => {
-      expect(getLinksFromMD.getLinksFromMD(`[example](www.example.com)`)).to.equal(true);
+    it('should have 1 link', () => {
+      expect(getLinksFromMD.getLinksFromMD(`[example](www.example.com)`)).to.have.lengthOf(1)
     });
   });
   describe('valid link from string - with another content', () => {
-      it('it should return true', () => {
-          expect(getLinksFromMD.getLinksFromMD(`[example](www.example.com) oieeee turu pom? [text](www.link.com)`)).to.equal(true);
-      });
+    it('should have 2 links', () => {
+      expect(getLinksFromMD.getLinksFromMD(`[example](www.example.com) oieeee turu pom? [text](www.link.com)`)).to.have.lengthOf(2);
+    });
   });
-  describe('invalid link from string', () => {
-      it('it should return false', () => {
-          expect(getLinksFromMD.getLinksFromMD(`(example)[www.example.com]`)).to.equal(false);
-      });
+  describe('string without links', () => {
+    it('should return empty', () => {
+      expect(getLinksFromMD.getLinksFromMD(`lalalala no link here`)).to.be.empty;
+    });
+  });
+  describe('empty string', () => {
+    it('should return empty', () => {
+      expect(getLinksFromMD.getLinksFromMD(``)).to.be.empty;
+    });
+  });
+  describe('string with invalid link', () => {
+    it('should return empty', () => {
+      expect(getLinksFromMD.getLinksFromMD(`invalid link here [example.com](invalid)`)).to.not.have.all.keys('text', 'href');
+    });
   });
 });
